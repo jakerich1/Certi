047f15f13c5ef580a933fe1cd670d4591c2c96ad
jakerich1 <jakeriches1@gmail.com>
2021-04-23 18:06:14 +0100

    Button styling

--- script.js
+++ script.js
@@ -7,16 +7,21 @@ $(function () {
 
 const control = document.querySelector(".control")
 const audioElement = document.querySelector("audio")
+const playbtn = document.querySelector("#play")
+const pausebtn = document.querySelector("#pause")
 let playing = false;
-
 
 const playMusic = () => {
     
     if (!playing) {
         audioElement.play()
+        playbtn.style.display = "none"
+        pausebtn.style.display = "block"
     }else{
         audioElement.pause();
         audioElement.currentTime = 0;
+        playbtn.style.display = "block"
+        pausebtn.style.display = "none"
     }
 
     playing = !playing
